ebf7eb5cda809433c0c526b439bd6aebadbb9f5f
Kevin Guthrie <kevin.guthrie@gmail.com>
2021-02-15 14:39:17 -0500

    Changing the implementation of parallel fibonacci so it doesn't spawn new tasks at every level, just the first 3. I realized that's what Patrick's is doing, and it makes sense, and it improves things dramatically :)

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Fibonacci LOL
 
 A response to [PK's GoFib](https://github.com/pdk/gofib) parallel go fibonacci which is a response to
-[this reddit thread](https://www.reddit.com/r/javascript/comments/lg80p2/nodejs_14_is_over_20x_faster_than_python38_for/gmq2s0b/). He was curious to see what a similar, concurrent version of a rust fibonacci number calculator would look like. This project reproduces the spirit of that request, so it doesn't do have any optimizations to try to get at the actual answer faster. 
+[this reddit thread](https://www.reddit.com/r/javascript/comments/lg80p2/nodejs_14_is_over_20x_faster_than_python38_for/gmq2s0b/). He was curious to see what a similar, concurrent version of a rust fibonacci number calculator would look like. This project reproduces the spirit of that request, so it doesn't do have any optimizations to try to get at the actual answer faster.
 
-This implementation explicitely performs both recursive executions at each step, so it is only a demonstration of exponential explosion in a bounded tokio runtime vs in a single native thread. Here are the results. Spoilers, doing this in tokio is not the right thing to do.
-   
+This implementation explicitly performs both recursive executions at each step, so it is only a demonstration of exponential explosion in a bounded tokio runtime vs in a single native thread. Here are the results. Parallelism is achieved by spawning tokio tasks for the recursive steps in the first levels which yields 2^3 = 8 threads to perform the standard fibonacci calculations in parallel
+
     cargo run --release 35
-    normal  : Ok(31.106 ms)   - 102334155
-    parallel: Ok(3.46844 s)   - 102334155 
+    normal  : 14.345ms - 9227465
+    parallel: 4.192ms  - 9227465
 
     cargo run --release 40
-    normal  : Ok(281.493 ms)  - 102334155
-    parallel: Ok(39.145676 s) - 102334155
-    
+    normal  : 154.561ms - 102334155
+    parallel: 43.028ms  - 102334155
+
     cargo run --release 47
-    normal  : Ok(6.48205 s)     - 2971215073
-    parallel: Ok(1596.095517 s) - 2971215073
-    
+    normal  : 4.479127s - 2971215073
+    parallel: 1.156553s - 2971215073
+
+    cargo run --release 55
+    normal  : 80.618475s - 53316291173
+    parallel: 21.005999s - 53316291173
+
--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,59 @@ use tokio::spawn;
 use std::env::args;
 use std::time::SystemTime;
 
-#[async_recursion::async_recursion]
-async fn fib_par(n: u64) -> u64 {
-    if n < 2 {
-        return n;
-    }
-
-    let v1_future = spawn(fib_par(n - 2));
-    let v2_future = spawn(fib_par(n - 1));
-
-    v1_future.await.unwrap() + v2_future.await.unwrap()
-}
-
+/// Baseline double-recursive fibonacci number calculation
 fn fib(n: u64) -> u64 {
-    if n < 2 {
-        return n;
+    if n <= 2 {
+        return 1;
     }
 
     fib(n - 1) + fib(n - 2)
 }
 
+/// Entry point for the parallel Fibonacci. This splits the load between 2
+/// tokio tasks
+async fn fib_par_0(n: u64) -> u64 {
+    if n <= 2 {
+        return 1;
+    }
+
+    let v1_future = spawn(fib_par_1(n - 1));
+    let v2_future = spawn(fib_par_1(n - 2));
+
+    v1_future.await.unwrap() + v2_future.await.unwrap()
+}
+
+/// Intermediate Fibonacci parallel function. This splits the load again between
+/// 2 more tokio tasks effectively brining the workers up to 4
+async fn fib_par_1(n: u64) -> u64 {
+    if n <= 2 {
+        return 1;
+    }
+
+    let v1_future = spawn(fib_par_2(n - 1));
+    let v2_future = spawn(fib_par_2(n - 2));
+
+    v1_future.await.unwrap() + v2_future.await.unwrap()
+}
+
+/// Final stage of the Fibonacci parallel function. This splits the load one
+/// final time to bring the total workers up to 8. All the remaining work will
+/// be done using the standard Fibonacci calculation function
+async fn fib_par_2(n: u64) -> u64 {
+    if n <= 2 {
+        return 1;
+    }
+
+    let v1_future = spawn(async move { fib(n - 1) });
+    let v2_future = spawn(async move { fib(n - 2) });
+
+    v1_future.await.unwrap() + v2_future.await.unwrap()
+}
+
 fn main() {
-    let n = args()
+
+    // Parse the fibonacci number ordinal form the from command line
+    let n : u64 = args()
         .skip(1) // skip the invocation
         .map(|n| n.parse::<u64>())
         .next()
@@ -33,21 +64,26 @@ fn main() {
         .ok()
         .expect("Failed to parse first argument as an non-negative integer");
 
+    // Run the single-threaded version
     {
         let start = SystemTime::now();
         let result = fib(n);
 
-        println!("normal  : {:?} - {}", start.elapsed(), result);
+        println!("normal  : {:?} - {}", start.elapsed().unwrap(), result);
     }
 
+    // Run the 8-thread parallel version
     {
-        let runtime = Builder::new_multi_thread().build().unwrap();
+        let runtime = Builder::new_multi_thread()
+            .worker_threads(8)
+            .build()
+            .unwrap();
 
         let start = SystemTime::now();
         let result = runtime
-            .block_on(runtime.spawn(fib_par(n)))
+            .block_on(runtime.spawn(fib_par_0(n)))
             .expect("Fibonacci calculation failed");
 
-        println!("parallel: {:?} - {}", start.elapsed(), result);
+        println!("parallel: {:?} - {}", start.elapsed().unwrap(), result);
     }
 }
